2664c0201c684ba757cb935096662c7b36a163df
Shahid Nabi <128903863+shahidnabi-exe@users.noreply.github.com>
2024-06-21 23:13:34 +0500

    initail commit

---
+++ todo.js
@@ -0,0 +1,61 @@
+
+
+const todolist=[ {
+    name:"Book Reading",
+    duedate: '12-3-24'
+},
+  {
+    name:"Playing Football",
+    duedate: '12-3-24'
+}];
+
+rendertodolist();
+
+function rendertodolist(){
+
+    let todolistHtml='';
+
+    for(let i=0; i<todolist.length; i++){       
+     
+        const todoobject=todolist[i];
+        const name= todoobject.name;    
+        const duedate=todoobject.duedate;
+        // const { name, duedate }= todoobject;
+        const html=`
+        <div>    ${name} </div>
+        <div> ${duedate} </div>
+        <button onclick="
+
+        todolist.splice(${i},1);
+        rendertodolist();
+        " class="delete-button">
+        Delete </button>
+        `; 
+        
+        todolistHtml+=html;
+    }
+
+
+    document.querySelector('.js-todo-lsit')
+    .innerHTML=todolistHtml;
+}
+
+
+
+
+function add(){
+   const inputElement= document.querySelector('.js-name-input');
+    const name= inputElement.value;
+    const dateinputElement=document.querySelector('.due-date');
+    const duedate= dateinputElement.value;
+    todolist.push({
+        name: name,
+        duedate: duedate
+        // name,
+        // duedate
+    });
+    inputElement.value='';
+
+    rendertodolist();
+
+}
